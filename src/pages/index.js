938d4ec00ea798a219e9ed1769b3fc000fac98ea
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-09-07 11:56:42 -0400

    feat: onSumbit function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,39 @@
 import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Home() {
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [shortenerUrl, setShortenerUrl] = useState('');
+  const [data, setData] = useState(null);
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    if (originalUrl.length >= 1 && shortenerUrl.length >= 1) {
+      setData({
+        url: originalUrl,
+        short: shortenerUrl,
+      });
+    }
+
+    fetch('/api/new', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
+
+    setOriginalUrl('');
+    setShortenerUrl('');
+  }
+
   return (
     <>
       <Head>
         <title>Url Shotenner</title>
       </Head>
       <div className="bg-slate-700 h-screen flex justify-center">
-        <div className="w-2/5 m-auto p-6 bg-slate-50 rounded-md">
+        <div className="w-2/5 m-auto p-9 bg-slate-50 rounded-md">
           <h1 className="text-4xl mb-10">Url Shortener</h1>
-          <form>
+          <form onSubmit={onSubmit}>
             <label
               className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-600"
               htmlFor="original-url"
@@ -17,6 +41,8 @@ export default function Home() {
               Original Url
             </label>
             <input
+              value={originalUrl}
+              onChange={(e) => setOriginalUrl(e.target.value)}
               className="transition duration-200 rounded-sm w-full mb-4 px-2 outline outline-offset-2 outline-blue-500 focus:scale-105"
               id="original-url"
               type="url"
@@ -31,6 +57,8 @@ export default function Home() {
               Shortener Url
             </label>
             <input
+              value={shortenerUrl}
+              onChange={(e) => setShortenerUrl(e.target.value)}
               className="trasition mb-7 duration-200 rounded-sm w-full px-2 outline outline-offset-2 outline-blue-500 focus:scale-105"
               id="original-url"
               type="text"
