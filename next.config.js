48740094facf2709dfd37c1b0cd30ab9cea84077
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-09-15 17:47:58 -0400

    feat: proxy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:hash',
+        destination: 'http://127.0.0.1:2000/:hash',
+      },
+      {
+        source: '/api/new',
+        destination: 'http://127.0.0.1:2000/new',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
