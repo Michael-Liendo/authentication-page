d064a2b2a77acce118fc06691460e695057ee24a
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-09-23 23:03:25 -0400

    fix: hash

--- src/pages/api/[hash].js
+++ src/pages/api/[hash].js
@@ -1,4 +1,4 @@
 export default async function handler(req, res) {
   const { hash } = req.query;
-  res.redirect(`${process.env.SERVER_URL}/${hash}`);
+  res.redirect(`${process.env.SERVER_URL}${hash}`);
 }
