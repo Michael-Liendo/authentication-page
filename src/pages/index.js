93d1ca23c68f6d02f99a2e7258f47cb78403da73
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-09-23 20:30:49 -0400

    feat: change button value

--- src/pages/index.js
+++ src/pages/index.js
@@ -128,7 +128,7 @@ export default function Home() {
             type="submit"
             className="trasition duration-200 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mt-6 mb-2 w-full"
           >
-            Shortener!!!
+            Create
           </button>
         </form>
       </>
